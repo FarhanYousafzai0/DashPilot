a72a4594de520a3a479a9572efd2575b24f65209
Farhan Yousafzai <coderwithferry@gmail.com>
2025-06-15 02:06:19 +0500

    Good Night

---
+++ app/Database/Controllers/UserController.js
@@ -0,0 +1,16 @@
+import { User } from "../Models/UserMode"
+
+
+
+
+
+
+export const getUsers = async()=>{
+
+
+const GetAllUsers =  awaitUser.find();
+
+ return GetAllUsers
+
+
+}
--- app/Database/Models/UserMode.js
+++ app/Database/Models/UserMode.js
@@ -1,14 +1,34 @@
 import mongoose from "mongoose";
 
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    role: {
+      type: Boolean,
+      default: false, 
+    },
+
+    status: {
+      type: Boolean,
+      default: true, 
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 
-
-
-export const UserSchema = mongoose.Schema({
-
-
-    
-
-
-
-})
+export const User = mongoose.models.User || mongoose.model("User", UserSchema);
